6084dd571d522ffa662c0fc6188f1e7bff54acf4
sadijho <sadej.jan1@gmail.com>
2026-05-20 19:57:31 +0200

    use mysql connection pool

--- magazyn-backend/config/db.js
+++ magazyn-backend/config/db.js
@@ -1,21 +1,27 @@
-//plik łączenia z bazą danych z pomocą zmiennych środowiskowych
+// Plik połączenia z bazą danych z pomocą zmiennych środowiskowych
 
 const mysql = require('mysql2');
 require('dotenv').config();
 
-const connection = mysql.createConnection({
+const pool = mysql.createPool({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0,
 });
 
-connection.connect((err) => {
+pool.getConnection((err, connection) => {
   if (err) {
     console.error('Błąd połączenia z bazą danych:', err);
     process.exit(1);
   }
+
   console.log('Połączono z bazą danych.');
+
+  connection.release();
 });
 
-module.exports = connection;
+module.exports = pool;
